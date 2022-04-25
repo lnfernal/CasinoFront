7c379e93e62a402330cc315cd1c09b8711f9dc32
mouchourider <mouchourider@gmail.com>
2022-04-25 11:33:12 +0300

    Nav-Bar style set

--- src/components/UI/NavButton.jsx
+++ src/components/UI/NavButton.jsx
@@ -3,7 +3,8 @@ import {useState} from "react";
 export default function NavButton (props) {
     const {text, style="nav_buttons mat-stroked-button", action=undefined, clicked} = props;
     const clickStyle = {
-        backgroundColor: "#db0000"
+        backgroundColor: "#db0000",
+        borderColor: "transparent",
     }
     return(
         <button className={style} style={clicked?clickStyle:{}} onClick={action}>
